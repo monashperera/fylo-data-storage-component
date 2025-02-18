f6119f0240f470a9c964a82908e6e3ee5e4b158c
Monash Perera <32927852+monashperera@users.noreply.github.com>
2025-02-18 16:00:12 +0530

    package.json homepage update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import InfoControls from './components/InfoControls';
 import StorageInfo from './components/StorageInfo';
+
 function App() {
   return (
     <div className="card-container">
-      
+
       <InfoControls />
       <StorageInfo />
 
